f054a3e1073f2db10861ba7b831a4820e19b5e63
OKKY-FIRMANSYAH-RAMADHAN <okkyfirmansyahr@gmail.com>
2024-05-07 08:54:32 +0700

    Halaman Dashboard Admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/administrator', [DashboardController::class, 'index'])->name('dashboard-admin');
